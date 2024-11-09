4b9e08b7c706eb3f5bdf18381a0b3b422c49e0de
AlexGS00 <acortojan@gmail.com>
2024-11-09 19:15:04 +0200

    Add more tags

--- pages/create/event.js
+++ pages/create/event.js
@@ -27,7 +27,14 @@ const CreateEventPage = () => {
         checkAuthStatus(setUser, setLoading, router);
     }, [router]);
 
-    const predefinedTags = ["Education", "Health", "Environment", "Community", "Technology"];
+    const predefinedTags = [
+        "Education", "Health", "Environment", "Community", "Technology",
+        "LGBTQ+", "Mental Health", "Climate Change", "Social Justice", 
+        "Diversity & Inclusion", "Women's Rights", "Human Rights", 
+        "Sustainability", "Youth Empowerment", "Equal Opportunity", 
+        "Gender Equality", "BLM", "Anti-Racism", "Religion", "Gender & Gender Identity", "Economic Diversity",
+        "Body Positivity", "Culture", "History"
+    ]
 
     useEffect(() => {
         authenticateUser();
