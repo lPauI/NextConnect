4e9e9a4e93e4728672f52ddb79b0959afe856c3d
AlexGS00 <acortojan@gmail.com>
2024-11-10 13:40:26 +0200

    change tags

--- utils/tags.js
+++ utils/tags.js
@@ -1,10 +1,10 @@
 // utils/tags.js
 export const predefinedTags = [
-    "Education", "Health", "Environment", "Community", "Technology",
-    "LGBTQ+", "Mental Health", "Climate Change", 
+    "LGBTQ+", "Disability Access", "Environment", "Community", 
+    "Mental Health", 
     "Diversity & Inclusion", "Women's Rights", "Human Rights", 
     "Sustainability", "Youth Empowerment", "Equal Opportunity", 
     "Gender Equality", "BLM", "Anti-Racism", "Gender & Gender Identity", 
     "Animal rights", "Social Justice", "Coming out", "Acceptance",
-    "Body Positivity", "Culture", "History", "Disability"
+    "Body Positivity", "Refugees & Immigrants", "Disability"
 ];
